409484781a4a0cfc6a6e5da0bf75989cd78ea2e2
hte-safouanejelassi <safouane.jelassi@hendrikthurau.enterprises>
2022-05-02 03:19:16 +0100

    set up the project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text("Hello World"),
+      ),
+    );
+  }
+}
+
+
